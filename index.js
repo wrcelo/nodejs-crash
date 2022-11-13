090ab8a010fabbbdc1e0225942d5c19ab665e430
wrcelo <marcelomramalho@gmail.com>
2022-11-13 19:11:29 -0300

    testing core modules - fs and path demo

---
+++ index.js
@@ -0,0 +1,3 @@
+const Person = require('./person');
+person1 = new Person('Marcelo', 21);
+person1.greeting();
---
+++ reference/fs_demo.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create folder
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created.');
+// });
+
+// Create and write to file
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello world!', err => {
+//     if (err) throw err;
+//     console.log('File written to...');
+// });
+// fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), ' I love nodejs!', err => {
+//     if (err) throw err;
+//     console.log('File written to...');
+// });
+
+// // Read file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// Rename a file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), err => {
+    if (err) throw err;
+    console.log('File renamed.');
+});
