c117ff810708494012035dd1cac06940b6a6a09a
wrcelo <marcelomramalho@gmail.com>
2022-11-15 19:42:09 -0300

    created a web server with node core modules

---
+++ logger.js
@@ -0,0 +1,21 @@
+const EventEmitter = require('events');
+const uuid = require('uuid');
+
+console.log(uuid.v4());
+
+class Logger extends EventEmitter {
+
+    log(msg) {
+        this.emit('message', { id: uuid.v4(), msg });
+    }
+}
+
+module.exports = Logger;
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called listener:', data));
+
+logger.log('Hello world!');
